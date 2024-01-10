f837d29d8ecc9e7ca1ae47e537597d4d6d813717
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2024-01-10 21:26:49 +0900

    DatePicker showDatePickerにはawaitを使用する macOSの場合、UIが独特

--- lib/main.dart
+++ lib/main.dart
@@ -30,58 +30,30 @@ class ChangeForm extends StatefulWidget {
 class _ChangeFormState extends State<ChangeForm> {
   final _formKey = GlobalKey<FormState>();
 
-  String _name = '';
-  String _email = '';
+  DateTime _date = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2016),
+        lastDate: DateTime.now().add(const Duration(days: 360)));
+    if (picked != null) setState(() => _date = picked);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Container(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              enabled: true,
-              maxLength: 10,
-              maxLengthEnforcement: MaxLengthEnforcement.none,
-              autovalidateMode: AutovalidateMode.disabled,
-              obscureText: false,
-              decoration: const InputDecoration(
-                  hintText: 'お名前を教えてください', labelText: '名前*'),
-              validator: (String? value) {
-                return value!.isEmpty ? '必須入力です' : null;
-              },
-              onSaved: (String? value) {
-                _name = value!;
-              },
-            ),
-            TextFormField(
-              maxLength: 100,
-              autovalidateMode: AutovalidateMode.disabled,
-              decoration: const InputDecoration(
-                  hintText: '連絡先を教えてください', labelText: 'メールアドレス*'),
-              validator: (String? value) {
-                return !value!.contains('@') ? 'アットマークがありません' : null;
-              },
-              onSaved: (String? value) {
-                _email = value!;
-              },
-            ),
-            ElevatedButton(onPressed: _submission, child: Text('保存'))
-          ],
-        ),
+    return Container(
+      padding: const EdgeInsets.all(50.0),
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Text("${_date}"),
+          ),
+          ElevatedButton(
+              onPressed: () => _selectDate(context), child: const Text('日付選択'))
+        ],
       ),
     );
-  }
-
-  void _submission() {
-    if (this._formKey.currentState?.validate() ?? false) {
-      this._formKey.currentState?.save();
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Processing Data')));
-      print(this._name);
-      print(this._email);
-    }
   }
 }
